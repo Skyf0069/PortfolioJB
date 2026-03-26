5e3ffce56ff366f4ec2fe67cfa552153125bfc7c
Lucas Skynium <125693476+Skyf0069@users.noreply.github.com>
2026-03-26 18:48:01 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  base: '/PortfolioJB/', // Pour que GitHub Pages trouve tes fichiers
+  // LE CHANGEMENT EST LÀ : on remet un simple slash
+  base: '/', 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
